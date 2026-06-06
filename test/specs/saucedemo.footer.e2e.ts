cd2e08cfee94844d4ca3067fe7f1b3287cfe807e
Yuriy99-developer <yuriy.hashuk95@gmail.com>
2026-06-06 21:52:23 +0300

    Fix types: accept ChainablePromiseElement in footer spec

--- test/specs/saucedemo.footer.e2e.ts
+++ test/specs/saucedemo.footer.e2e.ts
@@ -5,7 +5,6 @@ describe('SauceDemo Footer Links', () => {
     it('Footer links open valid targets', async () => {
         await LoginPage.open()
         await LoginPage.login('standard_user', 'secret_sauce')
-
         const twitter = await $('.social_twitter a')
         const facebook = await $('.social_facebook a')
         const linkedin = await $('.social_linkedin a')
@@ -13,5 +12,17 @@ describe('SauceDemo Footer Links', () => {
         await expect(twitter).toBeExisting()
         await expect(facebook).toBeExisting()
         await expect(linkedin).toBeExisting()
+
+        const assertLinkProps = async (el: ReturnType<typeof $>, expectedDomain: string) => {
+            const href = await el.getAttribute('href')
+            const target = await el.getAttribute('target')
+            await expect(href).toBeTruthy()
+            await expect(href).toContain(expectedDomain)
+            await expect(target).toBe('_blank')
+        }
+
+        await assertLinkProps(twitter, 'twitter.com')
+        await assertLinkProps(facebook, 'facebook.com')
+        await assertLinkProps(linkedin, 'linkedin.com')
     })
 })
